32278c06e091556045246c5f7137477f9b23d6af
ilknurtulgar <ilknur001-@hotmail.com>
2023-09-14 23:23:20 +0300

    first commit

---
+++ lib/core/base/base_state/base_state.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+
+abstract class BaseState<T extends StatefulWidget,M extends Store>
+    extends State<T> {
+  BaseState({
+    Key? key,
+  });
+
+  ThemeData get themeData => Theme.of(context);
+
+  ColorScheme get colorScheme => Theme.of(context).colorScheme;
+
+  TextTheme get textTheme => Theme.of(context).textTheme;
+
+  double get height => MediaQuery.of(context).size.height;
+
+  double get width => MediaQuery.of(context).size.width;
+
+  double get topPadding => MediaQuery.of(context).padding.top;
+
+  double get bottomPadding => MediaQuery.of(context).padding.bottom;
+
+  double dynamicHeight(double value) => (height - topPadding - bottomPadding) / 932 * value; // design height
+
+  double dynamicWidth(double value) => width / 430 * value; // design width
+}
+
+abstract class BaseStateless extends StatelessWidget {
+  const BaseStateless({Key? key}) : super(key: key);
+
+  ThemeData themeData(BuildContext context) => Theme.of(context);
+
+  ColorScheme colorScheme(BuildContext context) =>
+      Theme.of(context).colorScheme;
+
+  DividerThemeData dividerTheme(BuildContext context) =>
+      Theme.of(context).dividerTheme;
+
+  TextTheme textTheme(BuildContext context) => Theme.of(context).textTheme;
+
+  double height(BuildContext context) => MediaQuery.of(context).size.height;
+
+  double width(BuildContext context) => MediaQuery.of(context).size.width;
+
+  double topPadding(BuildContext context) => MediaQuery.of(context).padding.top;
+
+  double bottomPadding(BuildContext context) =>
+      MediaQuery.of(context).padding.bottom;
+
+  double dynamicHeight(BuildContext context, double value) =>
+      (height(context) - topPadding(context) - bottomPadding(context)) /
+      932 *
+      value; // design height
+
+  double dynamicWidth(BuildContext context, double value) =>
+      width(context) / 430 * value; // design width
+}
+
+extension BaseStateExtension on State {
+  DividerThemeData get dividerTheme => Theme.of(context).dividerTheme;
+
+  ThemeData get themeData => Theme.of(context);
+
+  ColorScheme get colorScheme => Theme.of(context).colorScheme;
+
+  TextTheme get textTheme => Theme.of(context).textTheme;
+
+  double height(BuildContext context) => MediaQuery.of(context).size.height;
+
+  double width(BuildContext context) => MediaQuery.of(context).size.width;
+
+  double topPadding(BuildContext context) => MediaQuery.of(context).padding.top;
+
+  double bottomPadding(BuildContext context) =>
+      MediaQuery.of(context).padding.bottom;
+
+  double dynamicHeight(BuildContext context, double value) =>
+      (height(context) - topPadding(context) - bottomPadding(context)) /
+      932 *
+      value; // design height
+
+  double dynamicWidth(BuildContext context, double value) =>
+      width(context) / 430 * value; // design width
+}
+
+extension StatelessExtension on StatelessWidget {
+  ThemeData themeData(BuildContext context) => Theme.of(context);
+
+  ColorScheme colorScheme(BuildContext context) =>
+      Theme.of(context).colorScheme;
+
+  DividerThemeData dividerTheme(BuildContext context) =>
+      Theme.of(context).dividerTheme;
+
+  TextTheme textTheme(BuildContext context) => Theme.of(context).textTheme;
+
+  double height(BuildContext context) => MediaQuery.of(context).size.height;
+
+  double width(BuildContext context) => MediaQuery.of(context).size.width;
+
+  double topPadding(BuildContext context) => MediaQuery.of(context).padding.top;
+
+  double bottomPadding(BuildContext context) =>
+      MediaQuery.of(context).padding.bottom;
+
+  double dynamicHeight(BuildContext context, double value) =>
+      (height(context) - topPadding(context) - bottomPadding(context)) /
+      932 *
+      value; // design height
+
+  double dynamicWidth(BuildContext context, double value) =>
+      width(context) / 430 * value; // design width
+}
---
+++ lib/core/managers/shared_preferences_manager.dart
@@ -0,0 +1,58 @@
+import "package:shared_preferences/shared_preferences.dart";
+
+class SharedPreferencesManager {
+  static final SharedPreferencesManager _instance =
+      SharedPreferencesManager._init();
+  static SharedPreferencesManager get instance => _instance;
+  SharedPreferences? _preferences;
+
+  SharedPreferencesManager._init() {
+    SharedPreferences.getInstance().then((value) => _preferences = value);
+  }
+
+  Future<void> preferencesInit() async {
+    _preferences ??= await SharedPreferences.getInstance();
+  }
+
+  Future<bool> set(String key, dynamic value) async {
+    switch (value.runtimeType) {
+      case String:
+        return _preferences!.setString(key, value as String);
+      case bool:
+        return _preferences!.setBool(key, value as bool);
+      case int:
+        return _preferences!.setInt(key, value as int);
+      case double:
+        return _preferences!.setDouble(key, value as double);
+      case (List<String>):
+        return _preferences!.setStringList(key, value as List<String>);
+      default:
+        return false;
+    }
+  }
+
+  Future<dynamic> get(Type type, String key) async {
+    switch (type) {
+      case String:
+        return _preferences!.getString(key);
+      case bool:
+        return _preferences!.getBool(key);
+      case int:
+        return _preferences!.getInt(key);
+      case double:
+        return _preferences!.getDouble(key);
+      case (List<String>):
+        return _preferences!.getStringList(key);
+      default:
+        return null;
+    }
+  }
+
+  Future<void> removeValue(String key) async {
+    await _preferences!.remove(key);
+  }
+
+  Future<void> clearAll() async {
+    await _preferences!.clear();
+  }
+}
