10a400f24682da79d3c9562794055766286ca8de
GizemGoksu <gizemgks_282@hotmail.com>
2023-09-15 20:45:57 +0300

    text form field added

---
+++ lib/product/component/text_form_field/text_obscurity/text_obscurity.g.dart
@@ -0,0 +1,32 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'text_obscurity.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$TextObscurity on TextObscurityBase, Store {
+  late final _$TextObscurityBaseActionController =
+      ActionController(name: 'TextObscurityBase', context: context);
+
+  @override
+  void changeVisibility() {
+    final _$actionInfo = _$TextObscurityBaseActionController.startAction(
+        name: 'TextObscurityBase.changeVisibility');
+    try {
+      return super.changeVisibility();
+    } finally {
+      _$TextObscurityBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+
+    ''';
+  }
+}
