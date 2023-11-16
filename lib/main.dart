ac0bb507ff006f6ce97a5715e4f7cfd460b69d8d
alisinancobani <ali@cobani.me>
2023-11-16 11:10:44 +0300

    Language manager added

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:calendar/screen/login/view/login_view.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
+import 'core/di/di.dart';
+import 'core/managers/localization_manager.dart';
 import 'core/modules/localization/language_type.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  await configureDependencies();
   runApp(
     EasyLocalization(
       supportedLocales: LanguageType.values.map((e) => e.getLocal).toList(),
@@ -16,8 +19,29 @@ Future<void> main() async {
   );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final LocalizationManager _localizationManager = getIt<LocalizationManager>();
+
+  @override
+  void didChangeDependencies() {
+    setLanguage();
+    super.didChangeDependencies();
+  }
+
+  void setLanguage() {
+    _localizationManager.getLanguage().then(
+          (value) => context.setLocale(
+            value.getLocal,
+          ),
+        );
+  }
 
   @override
   Widget build(BuildContext context) {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import devicelocale
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DevicelocalePlugin.register(with: registry.registrar(forPlugin: "DevicelocalePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
