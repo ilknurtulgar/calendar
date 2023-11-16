703136ac885219fe5e20013916d5dcee8ffc9e11
alisinancobani <ali@cobani.me>
2023-11-16 11:26:32 +0300

    removed MainApp from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:calendar/screen/login/view/login_view.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 import 'core/di/di.dart';
-import 'core/managers/localization_manager.dart';
+import 'core/init/init.dart';
 import 'core/modules/localization/language_type.dart';
 
 Future<void> main() async {
@@ -14,43 +13,9 @@ Future<void> main() async {
     EasyLocalization(
       supportedLocales: LanguageType.values.map((e) => e.getLocal).toList(),
       path: assetsPathLocalizations,
-      child: const MainApp(),
+      child: const Init(),
     ),
   );
 }
 
-class MainApp extends StatefulWidget {
-  const MainApp({super.key});
 
-  @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  final LocalizationManager _localizationManager = getIt<LocalizationManager>();
-
-  @override
-  void didChangeDependencies() {
-    setLanguage();
-    super.didChangeDependencies();
-  }
-
-  void setLanguage() {
-    _localizationManager.getLanguage().then(
-          (value) => context.setLocale(
-            value.getLocal,
-          ),
-        );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      home: const LoginView(),
-    );
-  }
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:calendar/core/init/init.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:calendar/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MainApp());
+    await tester.pumpWidget(const Init());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
